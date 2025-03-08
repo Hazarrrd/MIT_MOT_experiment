205e22153ffe3d4b85bd314b752600790dc96783
jsieradzki <jannsieradzki@gmail.com>
2025-03-08 18:26:28 +0100

    todo change

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ import random
 # case dla np. 2 MOT/MIT i guess pojawia się kompletnie gdzie indziej, bardzo łatwo, czy zmienic zeby tylko sąsiad był wybierany?
 # parametry do eksperymentu ustawienie
 # setup fizyczny eksperymentu, kamera, monitor, krzeslo, biurko, klawiatura, ustawienie reki
-# sniezynki - zrobilismy błąd że robilismy głosowanie na pojedyczne sniezki, a nie zestawy ze stałą różnicą odległości
+# sniezynki - zrobilismy błąd że robilismy głosowanie na pojedyczne sniezki, a nie zestawy ze stałą różnicą odległości + teraz mamy losowanie 10/12 zamiast 8/12
 
 ##### CONFIGURATION:
 #win_size = [2560, 1440] 
